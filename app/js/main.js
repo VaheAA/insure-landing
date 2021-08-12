ba11e343921ca1a358609679d162c43f4c2aa4c9
VaheAA <vahe.abov@gmail.com>
2021-08-12 21:02:19 +0400

    new

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,17 @@
 const hamburgerOpen = document.querySelector('.hamburger__open');
 const hamburgerClose = document.querySelector('.hamburger__close');
 const mobileMenu = document.querySelector('.menu');
+const footerIcons = document.querySelectorAll('.icon');
 
 hamburgerOpen.addEventListener('click', () => {
     hamburgerOpen.classList.toggle('hidden');
     mobileMenu.classList.toggle('hidden');
     hamburgerClose.classList.toggle('hidden');
-})
+});
 
 hamburgerClose.addEventListener('click', () => {
     hamburgerOpen.classList.toggle('hidden');
     mobileMenu.classList.toggle('hidden');
     hamburgerClose.classList.toggle('hidden');
-})
+});
 
